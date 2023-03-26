6e3b3384865696049d2bb2837a279b0c41920453
BigBobandy <jdykes207@gmail.com>
2023-03-26 12:55:20 -0400

    Update script.js
    added objects representing both the player and the enemy. Inside the objects are the health values and a function that can be used to update them

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+//Object representing the player in the game
+const player = {
+  health: 150,
+  maxHealth: 150,
+  element: document.getElementById("player-health"),
+  updateHealth: function (newHealth) {
+    this.health = newHealth;
+    this.element.textContent = newHealth;
+  },
+};
+
+//Object representing the enemy in the game
+const enemy = {
+  health: 150,
+  maxHealth: 150,
+  element: document.getElementById("enemy-health"),
+  updateHealth: function (newHealth) {
+    this.health = newHealth;
+    this.element.textContent = newHealth;
+  },
+};
+
 //Bringing in UI elements
 const beginBtn = document.getElementById("begin-btn");
 const rollBtn = docment.getElementById("roll-btn");
 const nextBtn = document.getElementById("next-btn");
-const playerHealthElement = document.getElementById("player-health");
-const enemyHealthElement = document.getElementById("enemy-health");
 const middleContainer = document.getElementById("middle-container");
 const turnCounterElement = document.getElementById("turn-counter");
 const whoseTurnElement = document.getElementById("whose-turn");
