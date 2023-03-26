ee57a737632e362b92977deddd13f95aaeabff57
BigBobandy <jdykes207@gmail.com>
2023-03-26 13:21:19 -0400

    updated html, css, and js
    Added a greeting message in the html and styled it in css. Added an event listener for the begin button that removes the gretting message when clicked, the begin button.  I also added a function that updates the turn counter element

--- script.js
+++ script.js
@@ -22,8 +22,32 @@ const enemy = {
 
 //Bringing in UI elements
 const beginBtn = document.getElementById("begin-btn");
-const rollBtn = docment.getElementById("roll-btn");
+const rollBtn = document.getElementById("roll-btn");
 const nextBtn = document.getElementById("next-btn");
 const middleContainer = document.getElementById("middle-container");
 const turnCounterElement = document.getElementById("turn-counter");
 const whoseTurnElement = document.getElementById("whose-turn");
+const greetingMessage = document.getElementById("greeting-message");
+
+let turnNumber = 1;
+
+function updateTurnCounter() {
+  turnCounterElement.textContent = turnNumber;
+}
+
+beginBtn.addEventListener("click", function () {
+  greetingMessage.remove();
+  beginBtn.classList.add("hide");
+  rollBtn.classList.remove("hide");
+
+  const rollMessage = document.createElement("p");
+  rollMessage.classList.add("message");
+  rollMessage.innerText =
+    "A dice must be rolled to decide who attacks first. The highest number goes first.";
+  middleContainer.appendChild(rollMessage);
+
+  player.updateHealth(player.health);
+  enemy.updateHealth(enemy.health);
+
+  updateTurnCounter();
+});
