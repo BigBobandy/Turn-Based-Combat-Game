8e7352b7b0ea393455991af45d82bed82caab915
BigBobandy <jdykes207@gmail.com>
2023-03-26 20:14:26 -0400

    updated script.js
    moved the diceRoll function inside of the player and enemy objects.  Finished the roll event handler and started working on the event handler for the next turn button.

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 const player = {
   health: 150,
   maxHealth: 150,
+  isItMyTurn: false,
   element: document.getElementById("player-health"),
   updateHealth: function (newHealth) {
     this.health = newHealth;
     this.element.textContent = newHealth;
+  },
+  diceRoll: function () {
+    return 1 + Math.floor(Math.random() * 20);
   },
 };
 
@@ -13,10 +17,14 @@ const player = {
 const enemy = {
   health: 150,
   maxHealth: 150,
+  isItMyTurn: false,
   element: document.getElementById("enemy-health"),
   updateHealth: function (newHealth) {
     this.health = newHealth;
     this.element.textContent = newHealth;
+  },
+  diceRoll: function () {
+    return 1 + Math.floor(Math.random() * 20);
   },
 };
 
@@ -28,42 +36,85 @@ const middleContainer = document.getElementById("middle-container");
 const turnCounterElement = document.getElementById("turn-counter");
 const whoseTurnElement = document.getElementById("whose-turn");
 const greetingMessage = document.getElementById("greeting-message");
-const battleInfoElement = document.getElementById("battle-info");
+const turnMessage = document.getElementById("battle-info");
 
+//Turn counter that is incremented and updated using the function below every turn
 let turnNumber = 1;
+let isItPlayerTurn;
+
+//This function is called when the page is loaded
+function startGame() {
+  //Sets the initial health values
+  player.updateHealth(player.health);
+  enemy.updateHealth(enemy.health);
+
+  updateTurnCounter();
+
+  //Attaching the event listeners
+  beginBtn.addEventListener("click", handleBeginClick);
+  rollBtn.addEventListener("click", handleRollClick);
+  nextBtn.addEventListener("click", handleNextTurnClick);
+}
+
+//Event Handler Functions
+function handleBeginClick() {
+  greetingMessage.remove();
+  beginBtn.classList.add("hide");
+  rollBtn.classList.remove("hide");
+
+  turnMessage.innerText =
+    "A dice must be rolled to decide who attacks first. The highest number goes first.";
+  middleContainer.appendChild(turnMessage);
+}
+
+function handleRollClick() {
+  playerRollResult = player.diceRoll();
+  enemyRollResult = enemy.diceRoll();
+
+  if (handleTieAndReroll(playerRollResult, enemyRollResult)) {
+    return; // exits the function early if there's a tie
+  }
+
+  if (playerRollResult > enemyRollResult) {
+    player.isItMyTurn = true;
+    turnMessage.innerText = `You rolled a ${playerRollResult} and the enemy rolled a ${enemyRollResult}. You get to attack first.`;
+  } else {
+    enemy.isItMyTurn = true;
+    turnMessage.innerText = `The enemy rolled a ${enemyRollResult} and you rolled a ${playerRollResult}. The enemy gets to go first.`;
+  }
+
+  middleContainer.appendChild(turnMessage);
+
+  rollBtn.classList.add("hide");
+  nextBtn.classList.remove("hide");
+  whoseTurnIsItAnyway();
+}
+
+function handleTieAndReroll(playerRoll, enemyRoll) {
+  if (playerRoll === enemyRoll) {
+    turnMessage.innerText = "It's a tie! Roll again.";
+    middleContainer.appendChild(turnMessage);
+    return true; //returns true if there is a tie
+  }
+  return false; //returns false if there is no tie
+}
+
+function handleNextTurnClick() {
+  turnNumber++;
+  updateTurnCounter();
+}
+
+//Calls the start game function
+startGame();
 
 function updateTurnCounter() {
   turnCounterElement.textContent = turnNumber;
 }
 
-function startGame() {
-  player.updateHealth(player.health);
-  enemy.updateHealth(enemy.health);
-}
-
-beginBtn.addEventListener("click", function () {
-  greetingMessage.remove();
-  beginBtn.classList.add("hide");
-  rollBtn.classList.remove("hide");
-
-  const rollMessage = document.createElement("p");
-  rollMessage.classList.add("message");
-  rollMessage.innerText =
-    "A dice must be rolled to decide who attacks first. The highest number goes first.";
-  middleContainer.appendChild(rollMessage);
-
-  updateTurnCounter();
-});
-
-rollBtn.addEventListener("click", function () {
-  playerRollResult = diceRoll();
-  enemyRollResult = diceRoll();
-  nextBtn.classList.remove("hide");
-  resultMessage = document.createElement("p");
-  resultMessage.innerText = `You rolled a ${playerRollResult} and the enemy rolled a ${enemyRollResult}.`;
-  middleContainer.appendChild(resultMessage);
-});
-
-function diceRoll() {
-  return 1 + Math.floor(Math.random() * 20);
+function whoseTurnIsItAnyway() {
+  if (player.isItMyTurn === true) {
+    whoseTurnElement.innerText = `Player's Turn`;
+  } else {
+    whoseTurnElement.innerText = `Enemy's Turn`;
+  }
 }
